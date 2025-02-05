ae3efd68aa63d9764f4b032ac04772217c1b106f
Shiirah <patrice.zheng.chen@gmail.com>
2025-02-05 10:31:05 +0100

    structure client user

--- cmd/clients/clients.go
+++ cmd/clients/clients.go
@@ -1,38 +1,46 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "github.com/gorilla/websocket"
+	"fmt"
+	"log"
+
+	"github.com/gorilla/websocket"
 )
 
+type User struct {
+	id       int
+	username string
+	pseudo   string
+	password string
+}
+
 func main() {
-    // Connexion au serveur WebSocket
-    url := "ws://localhost:8080"
-    conn, _, err := websocket.DefaultDialer.Dial(url, nil)
-    if err != nil {
-        log.Fatalf("Erreur de connexion au serveur WebSocket : %v", err)
-    }
-    defer conn.Close()
+	// Connexion au serveur WebSocket
+	url := "ws://localhost:8080"
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		log.Fatalf("Erreur de connexion au serveur WebSocket : %v", err)
+	}
+	defer conn.Close()
 
-    // Lire le message d'accueil
-    _, message, err := conn.ReadMessage()
-    if err != nil {
-        log.Fatalf("Erreur lors de la lecture du message : %v", err)
-    }
-    fmt.Printf("Message du serveur : %s\n", message)
+	// Lire le message d'accueil
+	_, message, err := conn.ReadMessage()
+	if err != nil {
+		log.Fatalf("Erreur lors de la lecture du message : %v", err)
+	}
+	fmt.Printf("Message du serveur : %s\n", message)
 
-    // Envoi d'un message au serveur
-    messageToSend := "Salut serveur !"
-    if err := conn.WriteMessage(websocket.TextMessage, []byte(messageToSend)); err != nil {
-        log.Fatalf("Erreur lors de l'envoi du message : %v", err)
-    }
-    fmt.Printf("Message envoyé : %s\n", messageToSend)
+	// Envoi d'un message au serveur
+	messageToSend := "Salut serveur !"
+	if err := conn.WriteMessage(websocket.TextMessage, []byte(messageToSend)); err != nil {
+		log.Fatalf("Erreur lors de l'envoi du message : %v", err)
+	}
+	fmt.Printf("Message envoyé : %s\n", messageToSend)
 
-    // Attente de la réponse du serveur
-    _, response, err := conn.ReadMessage()
-    if err != nil {
-        log.Fatalf("Erreur lors de la lecture de la réponse : %v", err)
-    }
-    fmt.Printf("Réponse du serveur : %s\n", response)
+	// Attente de la réponse du serveur
+	_, response, err := conn.ReadMessage()
+	if err != nil {
+		log.Fatalf("Erreur lors de la lecture de la réponse : %v", err)
+	}
+	fmt.Printf("Réponse du serveur : %s\n", response)
 }
